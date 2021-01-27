8630f2d317c2c7224d327e3e30fd367cc3344cf3
Admin <2003.leonid2003@gmail.com>
2021-01-27 21:06:20 +0300

    cool shit

--- app/src/main/java/spiridonov/no_to_garbage/descriptionMenu/DescriptionFragment.kt
+++ app/src/main/java/spiridonov/no_to_garbage/descriptionMenu/DescriptionFragment.kt
@@ -23,6 +23,7 @@ import kotlinx.android.synthetic.main.fragment_description.*
 import spiridonov.no_to_garbage.R
 import java.io.File
 
+
 class DescriptionFragment : Fragment() {
     private lateinit var mStorageRef: StorageReference
     private lateinit var msp: SharedPreferences
@@ -37,68 +38,79 @@ class DescriptionFragment : Fragment() {
         if (msp.contains("thing")) {
             mainCategory = msp.getString("thing", "").toString()
         }
+
+        val threadDescription = Thread {
+
+            val firebaseDate = FirebaseDatabase.getInstance()
+            val rootReference = firebaseDate.reference
+            val garbageReference = rootReference.child("GarbageInformation").child(mainCategory)
+
+            val headReference = garbageReference.child("head")
+            val bodyReference = garbageReference.child("body")
+
+            headReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {}
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val txt: String? = snapshot.getValue(String::class.java)
+                    if (txt != null) desc_head.text = txt
+                }
+            })
+
+            bodyReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {}
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val txt: String? = snapshot.getValue(String::class.java)
+                    if (txt != null) desc_body.text = txt.replace("_n", "\n")
+                }
+            })
+
+
+        }
+
+        threadDescription.start()
         val gallery = root.findViewById<LinearLayout>(R.id.trueGallery)
-        val inflater = LayoutInflater.from(context)
-        val threadPhoto = Thread {
-            mStorageRef = FirebaseStorage.getInstance().reference
-            val garbageRef: StorageReference =
-                mStorageRef.child("Garbage").child(mainCategory).child("0.png")
-            val view = inflater.inflate(R.layout.gallery_item, gallery, false)
-            val text = view.findViewById<TextView>(R.id.txtGallery)
-            text.text = ""
-            val img = view.findViewById<ImageView>(R.id.imgGallery)
-            img.setOnClickListener {
-                Toast.makeText(context, "Click!", Toast.LENGTH_SHORT).show()
-            }
+        val myinflater = LayoutInflater.from(context)
+
+        mStorageRef = FirebaseStorage.getInstance().reference
+        val garbageRef: StorageReference =
+            mStorageRef.child("Garbage").child(mainCategory)
 
 
-            val localFile = File.createTempFile("images", "png")
-            garbageRef.getFile(localFile)
+        garbageRef.listAll().addOnSuccessListener { result ->
+            result.items.forEach { imageRef ->
+                val view = myinflater.inflate(R.layout.gallery_item, gallery, false)
+                val text = view.findViewById<TextView>(R.id.txtGallery)
+                text.text = ""
+                val img = view.findViewById<ImageView>(R.id.imgGallery)
+                img.setOnClickListener {
+                    it
+                    Toast.makeText(context, "Click on ${it.id}!", Toast.LENGTH_SHORT).show()
+                }
+                
+                val localFile = File.createTempFile("images", "png")
+                imageRef.getFile(localFile)
                     .addOnSuccessListener {
                         // Successfully downloaded data to local file
                         val bitmap = BitmapFactory.decodeFile(localFile.path)
                         img.setImageBitmap(bitmap)
+
                     }.addOnFailureListener {
                         // Handle failed download
                         // ...
+
                     }.addOnProgressListener {
                         Log.d("Progress", "Download")
                     }
+
                 gallery.addView(view)
-
-
+            }
         }
 
 
-        val threadDescription = Thread {
-
-        val firebaseDate = FirebaseDatabase.getInstance()
-                val rootReference = firebaseDate.reference
-                val garbageReference = rootReference.child("GarbageInformation").child(mainCategory)
-
-                val headReference = garbageReference.child("head")
-                val bodyReference = garbageReference.child("body")
-
-                headReference.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onCancelled(error: DatabaseError) {}
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val txt: String? = snapshot.getValue(String::class.java)
-                        if (txt != null) desc_head.text = txt
-                    }
-                })
-
-                bodyReference.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onCancelled(error: DatabaseError) {}
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val txt: String? = snapshot.getValue(String::class.java)
-                        if (txt != null) desc_body.text = txt.replace("_n", "\n")
-                    }
-                })
 
 
-        }
-        threadPhoto.start()
-        threadDescription.start()
+
+
         return root
     }
 }
