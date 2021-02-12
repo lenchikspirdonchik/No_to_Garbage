bc377e6790bcd6ddbfe724b4d7674ff9ecf22f36
lenchik <2003.leonid2003@gmail.com>
2021-02-12 19:10:02 +0300

    cool shit

--- app/src/main/java/spiridonov/no_to_garbage/DatabaseFragment.kt
+++ app/src/main/java/spiridonov/no_to_garbage/DatabaseFragment.kt
@@ -5,8 +5,7 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import java.sql.Connection
@@ -22,6 +21,43 @@ class DatabaseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_database, container, false)
+
+        val allGarbage = arrayOf(
+            getString(R.string.BTN_Jars),
+            getString(R.string.BTN_Bottles),
+            getString(R.string.BTN_Сontainers),
+            getString(R.string.BTN_Box),
+            getString(R.string.BTN_GoodClothes),
+            getString(R.string.BTN_BadClothes),
+            getString(R.string.BTN_Battery),
+            getString(R.string.BTN_Paper),
+            getString(R.string.BTN_Technic)
+        )
+        val spinner = root.findViewById<Spinner>(R.id.spinnerGarbageDB)
+        val adaptermain: ArrayAdapter<String> =
+            ArrayAdapter<String>(
+                requireActivity(),
+                R.layout.support_simple_spinner_dropdown_item,
+                allGarbage
+            )
+        adaptermain.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adaptermain
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                itemSelected: View, selectedItemPosition: Int, selectedId: Long
+            ) {
+                buildCard(root, allGarbage[selectedItemPosition])
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        return root
+    }
+
+
+    private fun buildCard(root: View, category: String) {
         val mAuth = FirebaseAuth.getInstance()
         val uuid = mAuth.currentUser?.uid
 
@@ -31,9 +67,9 @@ class DatabaseFragment : Fragment() {
         val password = "H+4ynXm20/5Yf-T"
 
         val handler = Handler()
-        val recyclerView = root.findViewById<LinearLayout>(R.id.myLin)
+        val linearLayout = root.findViewById<LinearLayout>(R.id.myLin)
         val myinflater = LayoutInflater.from(context)
-
+        linearLayout.removeAllViews()
 
         val p = Properties()
         p.setProperty("user", user)
@@ -48,7 +84,7 @@ class DatabaseFragment : Fragment() {
 
                 if (uuid != null) {
                     val rs: ResultSet =
-                        st.executeQuery(" select * from no2garbage where category='Батарейки' AND uuid='${uuid}' order by date desc")
+                        st.executeQuery(" select * from no2garbage where category='$category' AND uuid='$uuid' order by date desc")
 
                     while (rs.next()) {
                         val dbdate: String = rs.getString("date").toString()
@@ -57,14 +93,14 @@ class DatabaseFragment : Fragment() {
 
                         handler.post {
                             val view =
-                                myinflater.inflate(R.layout.note_card, recyclerView, false)
+                                myinflater.inflate(R.layout.note_card, linearLayout, false)
                             val date = view.findViewById<TextView>(R.id.note_date)
                             val category = view.findViewById<TextView>(R.id.note_category)
                             val amount = view.findViewById<TextView>(R.id.note_amount)
                             date.text = dbdate
                             category.text = dbcategory
                             amount.text = dbamount
-                            recyclerView.addView(view)
+                            linearLayout.addView(view)
 
                         }
                     }
@@ -77,11 +113,7 @@ class DatabaseFragment : Fragment() {
 
         }
         thread.start()
-
-
-        return root
     }
-
 
 }
 
