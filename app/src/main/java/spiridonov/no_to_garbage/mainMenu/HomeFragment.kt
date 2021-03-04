5a0927d6aefb241248607d24584e34c35b3d9f56
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-03-04 23:19:39 +0300

    cool shit

--- app/src/main/java/spiridonov/no_to_garbage/mainMenu/HomeFragment.kt
+++ app/src/main/java/spiridonov/no_to_garbage/mainMenu/HomeFragment.kt
@@ -4,17 +4,15 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.*
-import androidx.core.view.marginRight
 import androidx.core.view.setMargins
 import androidx.fragment.app.Fragment
-import okhttp3.TlsVersion
 import spiridonov.no_to_garbage.R
 
 
@@ -25,95 +23,78 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root: View = inflater.inflate(R.layout.fragment_home, container, false)
-
+        val displaymetrics = DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(displaymetrics)
+        val screenWidth = displaymetrics.widthPixels
+        val screenHeight = displaymetrics.heightPixels
+        val category = arrayOf("Кухня", "Ванная", "Гардеробная", "Кабинет")
         val layout = root.findViewById<LinearLayout>(R.id.linearMain)
-//TableRow.LayoutParams
-        val TLP = TableLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-        TLP.setMargins(15)
+        val TLP = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        val TRP = TableRow.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        val LP = TableLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        LP.setMargins(15)
+        var id = 1
         val table = TableLayout(context)
         table.layoutParams = TLP
-        val handle = Handler()
         layout.addView(table)
-        for (j in 0..10) {
-            val thread = Thread {
-                val trName = TableRow(context)
-                val trPhoto = TableRow(context)
-                val textCategory = TextView(context)
-                textCategory.setTextColor(Color.BLACK)
-                textCategory.textSize = 18F
-                table.layoutParams = TLP
+        for (name in category) {
+            val trName = TableRow(context)
+            val trPhoto = TableRow(context)
+            trPhoto.layoutParams = LP
+            val textCategory = TextView(context)
+            textCategory.setTextColor(Color.BLACK)
+            textCategory.textSize = 22F
+            table.layoutParams = TLP
 
-                textCategory.text = "\ncategory, j = $j"
-                trName.addView(textCategory)
-                val horizontalScrollView = HorizontalScrollView(context)
-                val linearscroll = LinearLayout(context)
-                linearscroll.orientation = LinearLayout.HORIZONTAL
-                horizontalScrollView.addView(linearscroll)
-                trPhoto.addView(horizontalScrollView)
-                for (i in 0..10) {
-                    val imageView = ImageView(context)
-                    val bMap = BitmapFactory.decodeResource(resources, R.drawable.battery)
-                    val bMapScaled = Bitmap.createScaledBitmap(bMap, 200, 200, true)
-                    imageView.setImageBitmap(bMapScaled)
-                    imageView.layoutParams = TLP
-                    linearscroll.addView(imageView)
+            textCategory.text = "${name}"
+            trName.addView(textCategory)
+            val horizontalScrollView = HorizontalScrollView(context)
+            horizontalScrollView.layoutParams = TRP
+
+            val linearscroll = LinearLayout(context)
+            linearscroll.orientation = LinearLayout.HORIZONTAL
+            horizontalScrollView.addView(linearscroll)
+            trPhoto.addView(horizontalScrollView)
+            val photocategory = setCategory(name)
+            for (i in photocategory) {
+                val resID = resources.getIdentifier(i, "drawable", context?.packageName)
+
+                val imageView = ImageView(context)
+                val bMap = BitmapFactory.decodeResource(resources, resID)
+                val bMapScaled = Bitmap.createScaledBitmap(
+                    bMap,
+                    screenWidth / 3 + 30, screenHeight / 5, true
+                )
+                imageView.setImageBitmap(bMapScaled)
+                imageView.layoutParams = LP
+                imageView.id = id
+                imageView.tag = id
+                imageView.setTag(imageView.id)
+                imageView.setOnClickListener { it
+                    val tag: String = it.tag.toString()
+                    Toast.makeText(requireContext(), tag, Toast.LENGTH_SHORT).show()
+
                 }
-
-
-                handle.post {
-                    table.addView(trName)
-                    table.addView(trPhoto)
-                }
-            }
-            thread.start()
-        }
-
-
-        /*
-        val kitchen = root.findViewById<Button>(R.id.btn_kitchen)
-        val bath = root.findViewById<Button>(R.id.btn_bathroom)
-        val cabinet = root.findViewById<Button>(R.id.btn_cabinet)
-        val wardrope = root.findViewById<Button>(R.id.btn_wardrope)
-        val btnMain = View.OnClickListener { v ->
-            val rotate: Animation = AnimationUtils.loadAnimation(context, R.anim.rotate)
-            v.startAnimation(rotate)
-            val mintent = Intent(context, ThingsActivity::class.java)
-            val btn: Button = v as Button
-            mintent.putExtra("KEY_CATEGORY", btn.text)
-            startActivity(mintent)
-
-        }
-
-
-        val naviga_hom = root.findViewById<BottomNavigationView>(R.id.naviga_home)
-        naviga_hom.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_statistics -> {
-                    val mintent = Intent(context, StatisticsActivity::class.java)
-                    startActivity(mintent)
-                }
-                R.id.navigation_addGarbage -> {
-                    val mintent = Intent(context, AddGarbageActivity::class.java)
-                    startActivity(mintent)
-                }
-                R.id.navigation_MapHome -> {
-                    val mintent = Intent(context, AllMapsActivity::class.java)
-                    startActivity(mintent)
-                }
+                linearscroll.addView(imageView)
+                id++
             }
 
-            false
+            table.addView(trName)
+            table.addView(trPhoto)
+
         }
-
-
-        kitchen.setOnClickListener(btnMain)
-        bath.setOnClickListener(btnMain)
-        cabinet.setOnClickListener(btnMain)
-        wardrope.setOnClickListener(btnMain)*/
-
 
         return root
 
     }
 
+    private fun setCategory(mainCategory: String): Array<String> {
+        return when (mainCategory) {
+            "Кухня" -> arrayOf("jars", "kitchenbottles", "containers", "box")
+            "Ванная" -> arrayOf("bathbottles")
+            "Гардеробная" -> arrayOf("badclothes", "goodclothes")
+            "Кабинет" -> arrayOf("battery", "paper", "technic")
+            else -> arrayOf("battery", "paper", "technic")
+        }
+    }
 }
